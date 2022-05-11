6aad5c8d7fc6459c590d298e81458db9eb73a4f7
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2022-05-11 17:12:55 +0900

    Update Test.java

--- src/main/java/persistence/dao/Test.java
+++ src/main/java/persistence/dao/Test.java
@@ -16,13 +16,10 @@ public class Test {
         sportsFacilitiesDTO.setSportsFacilitiesTel("010-9999-8888");
         sportsFacilitiesDTO.setSportsFacilitiesTypeCode("1");
         sportsFacilitiesDTO.setLocalInfoIndex(16679);
-jhghuyihiu
         sportsFacilitiesDAO.insertSportsFacilities(sportsFacilitiesDTO);
         List<SportsFacilitiesDTO> list = sportsFacilitiesDAO.selectAllSportsFacilities();
         for(int i=0; i<list.size(); i++) {
             System.out.println(list.get(i));
         }
     }
-dasdssdasfdw
-}dsdas
-d
+}
