a3cfc4b68e684ba7e94a8b1590b4ea09608cb951
hj-8888 <xcxc4189@naver.com>
2022-05-18 19:59:20 +0900

    1

--- src/main/java/persistence/dao/Test.java
+++ src/main/java/persistence/dao/Test.java
@@ -1,5 +1,23 @@
 package persistence.dao;
 
+import persistence.dto.BulletinDTO;
+
+import java.util.Date;
+
 public class Test {
+
+
+    public static void main(String[] args) {
+        Date date = new Date(System.currentTimeMillis());
+        BulletinDAO bulletinDAO = new BulletinDAO();
+        BulletinDTO bulletinDTO = new BulletinDTO();
+        bulletinDTO.setBulletinTitle("그그그금오");
+        bulletinDTO.setBulletinContent("내내요요용");
+        bulletinDTO.setAttachedPicture("사사진진");
+        bulletinDTO.setBulletinPW("비비번번");
+        bulletinDTO.setMemberIndex(1);
+        bulletinDAO.insertBulletin(bulletinDTO);
+
+    }
 
 }
